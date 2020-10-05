f58eb7975e3dad1de95582f464b8f5ff60efbb0e
Jeremy Lampron <lamj1406@usherbrooke.ca>
2020-10-05 14:57:47 -0400

    code charlookup

--- Code/charLookup.c
+++ Code/charLookup.c
@@ -1,3 +1,25 @@
+#include <stdio.h>
+
+int charLookup (char c, char* str);
+
+
 int main(void){
+	
+	int position = charLookup( 'm', "canard");
+	
+	printf( "%d", position);
+	
     return 0;
+}
+
+int charLookup (char c, char* str)
+{
+	for (int index = 0; str[index] != '\0'; index ++)
+	{
+		if (str[index] == c )
+		{	
+			return index;
+		}
+	}
+	return -1;
 }
