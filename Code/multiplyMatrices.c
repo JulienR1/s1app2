779b89d55889e6f23d53e351a8aa89400bb851a0
Julien <julrousseau20@gmail.com>
2020-10-06 12:53:22 -0400

    Fix de sin et cos

--- Code/multiplyMatrices.c
+++ Code/multiplyMatrices.c
@@ -5,32 +5,80 @@
 void multiplyMatrices(int mat1[], int mat2[], int matOut[]);
 void printMat(int mat[]);
 
-int main(void){
-    
-    int matOut[SIZE * SIZE] = {0,0,0,0,0,0,0,0,0};
-    int mat1[SIZE * SIZE] = {1,2,3,4,5,6,7,8,9};
-    int mat2[SIZE * SIZE] = {-1,0,0,0,-1,0,0,0,-1};
-    multiplyMatrices(mat1, mat2, matOut);
-    printMat(matOut);
+void test();
 
+int main(void)
+{
+    test();
     return 0;
 }
 
-void multiplyMatrices(int mat1[], int mat2[], int matOut[]){
-    for(int i = 0; i < SIZE * SIZE; i++){
-        for(int n = 0; n < SIZE; n++){
+void multiplyMatrices(int mat1[], int mat2[], int matOut[])
+{
+    for (int i = 0; i < SIZE * SIZE; i++)
+    {
+        for (int n = 0; n < SIZE; n++)
+        {
             matOut[i] += mat1[n + (int)(i / SIZE) * SIZE] * mat2[SIZE * n + i % SIZE];
         }
     }
 }
 
-void printMat(int mat[]){
+void printMat(int mat[])
+{
     printf("[");
-    for(int i = 0; i < SIZE * SIZE; i++){
+    for (int i = 0; i < SIZE * SIZE; i++)
+    {
         printf("%d", mat[i]);
-        if(i < SIZE * SIZE - 1){
+        if (i < SIZE * SIZE - 1)
+        {
             printf(",");
         }
     }
     printf("]");
+}
+
+void test()
+{
+    printf("\n\n-- Tests de la fonction 'multiplyMatrices(int mat1[], int mat2[], int matOut[])' --\n");
+    printf("Entrees\t\t\t\t\t\tValeur attendue\t\tValeur obtenue\t\tTest reussi\n");
+
+    const int multiplyMatTestCount = 4;
+    int mat1Inputs[][SIZE * SIZE] = {
+        {1, 1, 1, 1, 1, 1, 1, 1, 1},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9}};
+    int mat2Inputs[][SIZE * SIZE] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 0, 0, 0, 1, 0, 0, 0, 1},
+        {0, 0, 1, 0, 1, 0, 1, 0, 0},
+        {-1, 0, 0, 0, -1, 0, 0, 0, -1}};
+    int matOutResult[][SIZE * SIZE] = {
+        {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 2, 3, 4, 5, 6, 7, 8, 9},
+        {3, 2, 1, 6, 5, 4, 9, 8, 7},
+        {-1, -2, -3, -4, -5, -6, -7, -8, -9}};
+
+    for (int i = 0; i < multiplyMatTestCount; i++)
+    {
+        int actualResult[SIZE * SIZE] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+        multiplyMatrices(mat1Inputs[i], mat2Inputs[i], actualResult);
+        char *successState = "OUI";
+        for (int j = 0; j < SIZE * SIZE; j++)
+        {
+            if (matOutResult[i][j] != actualResult[j])
+            {
+                successState = "NON";
+            }
+        }
+        printMat(mat1Inputs[i]);
+        printf(", ");
+        printMat(mat2Inputs[i]);
+        printf("\t");
+        printMat(matOutResult[i]);
+        printf("\t");
+        printMat(actualResult);
+        printf("\t%s\n", successState);
+    }
 }
--- Code/sin.c
+++ Code/sin.c
@@ -1,85 +1,134 @@
 #include <stdio.h>
 
-unsigned int factorial(unsigned int n);
-double pow(double base, int exponent);
+double factorial(unsigned int n);
+long double fpow(double base, int exponent);
 double fmod(double n, double modulo);
-double sin(double angle);
+long double fsin(double angle);
 
-void test(double expected, double result);
+void test();
 
 #define PI 3.14159
-#define EPSILON 0.00001
+#define ITERATION_COUNT 10
 
-int main(void){
-    test(factorial(0), 1);
-    test(factorial(1), 1);
-    test(factorial(5), 120);
-    puts("");
-
-    test(pow(2,2), 4);
-    test(pow(0.5,2), 0.25);
-    test(pow(2,-2), 0.25);
-    test(pow(0,2), 0);
-    test(pow(2,0), 1);
-    puts("");
-
-    test(fmod(10,3.33), 0.003);
-    test(fmod(10,PI), 0.18);
-    test(fmod(10,-PI), -0.18);
-    puts("");
-
-    test(sin(0), 0);
-    test(sin(PI), 0);
-    test(sin(PI / 2.0), 1);
-    test(sin(3*PI/2.0), -1);
-    test(sin(7*PI/2.0), -1);
-    test(sin(PI/6.0), 0.5);
-    test(sin(PI/3.0), 0.866);
-    test(sin(651), -0.6367);
-
+int main(void)
+{
+    test();
     return 0;
 }
 
-unsigned int factorial(unsigned int n){
-    unsigned int result = 1;
-    for(; n > 1; n--){
+double factorial(unsigned int n)
+{
+    double result = 1;
+    for (; n > 1; n--)
+    {
         result *= n;
     }
     return result;
 }
 
-double pow(double base, int exponent){
-    if(exponent < 0){
+long double fpow(double base, int exponent)
+{
+    if (exponent < 0)
+    {
         exponent = -exponent;
         base = 1 / base;
     }
 
-    double result = 1;
-    for(int i = 0; i < exponent; i++){
+    long double result = 1;
+    for (int i = 0; i < exponent; i++)
+    {
         result *= base;
     }
 
     return result;
 }
 
-double fmod(double n, double modulo){
+double fmod(double n, double modulo)
+{
     return n / modulo - (int)(n / modulo);
 }
 
-double sin(double angle){
+long double fsin(double angle)
+{
     angle = fmod(angle, 2 * PI) * 2 * PI;
 
-    int iteration = 1;
-    double result = 0, previousResult = 0;
-    do {
-        previousResult = result;
-        result += pow(-1, iteration + 1) * pow(angle, 2 * iteration - 1) / factorial(2 * iteration - 1);
+    int iteration = 0;
+    long double result = 0;
+    do
+    {
+        result += fpow(-1, iteration) * fpow(angle, 2 * iteration + 1) / factorial(2 * iteration + 1);
         iteration++;
-    } while(result - previousResult < EPSILON && result - previousResult > -EPSILON);
-    
+    } while (iteration < ITERATION_COUNT);
     return result;
 }
 
-void test(double expected, double result){
-    printf("%.4f\t\t%.4f\t\t%s\n", expected, result, expected == result ? "OK" : "FAIL");
+void test()
+{
+    // Factorial
+    printf("\n\n-- Tests de la fonction 'factorial(int n)' --\n");
+    printf("Entrees\tValeur attendue\tValeur obtenue\tTest reussi\n");
+
+    int factorialTestCount = 3;
+    int factorialInputs[] = {0, 1, 5};
+    int factorialResults[] = {1, 1, 120};
+
+    for (int i = 0; i < factorialTestCount; i++)
+    {
+        int actualResult = factorial(factorialInputs[i]);
+        char *successState = factorialResults[i] == actualResult ? "OUI" : "NON";
+        printf("%d\t%d\t\t%d\t\t%s\n", factorialInputs[i], factorialResults[i], actualResult, successState);
+    }
+
+    printf("\n");
+
+    // Power
+    printf("\n\n-- Tests de la fonction 'pow(double base, int exponent)' --\n");
+    printf("Entrees\t\tValeur attendue\tValeur obtenue\tTest reussi\n");
+
+    int powTestCount = 5;
+    double powBaseInputs[] = {2, 0.5, 2, 0, 2};
+    int powExponentInputs[] = {2, 2, -2, 2, 0};
+    double powResults[] = {4, 0.25, 0.25, 0, 1};
+
+    for (int i = 0; i < powTestCount; i++)
+    {
+        double actualResult = fpow(powBaseInputs[i], powExponentInputs[i]);
+        char *successState = powResults[i] == actualResult ? "OUI" : "NON";
+        printf("%.3f, %d\t%.3f\t\t%.3f\t\t%s\n", powBaseInputs[i], powExponentInputs[i], powResults[i], actualResult, successState);
+    }
+
+    printf("\n");
+
+    // decimal modulus
+    printf("\n\n-- Tests de la fonction 'fmod(double n, double modulo)' --\n");
+    printf("Entrees\t\tValeur attendue\t\tValeur obtenue\tTest reussi\n");
+
+    int fmodTestCount = 3;
+    double nInputs[] = {10, 10, 10};
+    double moduloInputs[] = {3.33, PI, -PI};
+    double fmodResults[] = {0.003, 0.1831, -0.1831};
+
+    for (int i = 0; i < fmodTestCount; i++)
+    {
+        double actualResult = fmod(nInputs[i], moduloInputs[i]);
+        char *successState = actualResult - fmodResults[i] < 0.0001 ? "OUI" : "NON";
+        printf("%.4f, %.4f\t\t%.4f\t\t%.4f\t\t%s\n", nInputs[i], moduloInputs[i], fmodResults[i], actualResult, successState);
+    }
+
+    printf("\n");
+
+    // sin
+    printf("\n\n-- Tests de la fonction 'sin(double angle)' --\n");
+    printf("Entrees\t\tValeur attendue\tValeur obtenue\tTest reussi\n");
+
+    int sinTestCount = 8;
+    double sinInputs[] = {0.0, PI, PI / 2.0, 3 * PI / 2.0, 7 * PI / 2.0, PI / 6.0, PI / 3.0, 651.0};
+    double sinResults[] = {0, 0, 1, -1, -1, 0.5, 0.866, -0.6367};
+
+    for (int i = 0; i < sinTestCount; i++)
+    {
+        double actualResult = fsin(sinInputs[i]);
+        char *successState = (sinResults[i] - actualResult < 0.0001) ? "OUI" : "NON";
+        printf("%.4f\t\t%.4f\t\t%.4f\t\t%s\n", sinInputs[i], sinResults[i], actualResult, successState);
+    }
 }
